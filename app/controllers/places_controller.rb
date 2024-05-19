cd63238ac01e16e47434effd051c68442deeca02
Divyansh Singh <divyansh.singh@kellogg.northwestern.edu>
2024-05-19 02:33:24 +0000

    xx

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,12 @@
 class PlacesController < ApplicationController
-end
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @places = Place.find_by({"id" => params["id"]})
+    @entries = Entry.where({ "place_id" => @placed["id"]})
+  end
+
+  
+
