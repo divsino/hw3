039524ab3007475b07b6984b870b3c867fb31e4e
Divyansh Singh <divyansh.singh@kellogg.northwestern.edu>
2024-05-19 02:38:48 +0000

    xx

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,33 @@
 class PlacesController < ApplicationController
   def index
+    # find all places
     @places = Place.all
+    # render places/index view
   end
 
-  def show
-    @places = Place.find_by({"id" => params["id"]})
-    @entries = Entry.where({ "place_id" => @placed["id"]})
-  end
 
+def show
+  # find a place
+  @places = Place.find_by({"id" => params["id"]})
+  # find related entries
+  @entries = Entry.where({"place_id" => @places["id"]})
+end
+
+def new
+  # render view with new place form
+end
+
+def create
+  # start with a new place
+  @place = Place.new
+  # assign user-entered form data to Place's columns
+  @place["name"] = params["name"]
   
+  # save Place row
+  @place.save
+  # redirect user
+  redirect_to "/places"
+end
+end
+
 
